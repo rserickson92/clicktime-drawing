1576508d2f77c06022601363c825a00a260e6524
Ryan Erickson <rserickson92@gmail.com>
2015-04-17 15:38:34 -0700

    add basic console logging for user connections

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require('express'),
+  app = express(),
+  http = require('http').Server(app),
+  io = require('socket.io')(http);
+app.use("/javascripts", express.static(__dirname + '/javascripts'));
 
 app.get('/', function(req, res){
-    res.sendfile('index.html');
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
 
 http.listen(3000, function(){
-    console.log('listening on *:3000');
+  console.log('listening on *:3000');
 });
