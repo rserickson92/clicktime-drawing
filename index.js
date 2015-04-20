11b0f5acda5bdac1a43a662398b90c47efec75d8
Ryan Erickson <rserickson92@gmail.com>
2015-04-19 20:58:38 -0700

    add randomized colors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+  fs = require('fs'),
   app = express(),
   http = require('http').Server(app),
   io = require('socket.io')(http);
@@ -6,13 +7,25 @@ app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendFile('index.html', {root: __dirname});
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.emit('color-load', selectColor());
 });
+
+var loadColors = function() {
+  var data = JSON.parse(fs.readFileSync('colors.json', 'utf-8'));
+  return data.colors;
+}
+
+colors = loadColors();
+selectColor = function() {
+  return colors.length > 0 ? colors.pop() : 'black';
+};
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
